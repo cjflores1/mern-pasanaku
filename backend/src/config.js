39a58850904cd60ca5d03b93c201e8adc257b305
Charles Flores <Charles.Flores@jala.university>
2024-03-23 01:07:14 -0400

    Updated route

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-// const DB_HOST = process.env.DB_HOST || "mongodb+srv://charlesflorescalle:gkicdK2VPIDZ0Q0h@cluster0.saz6nxn.mongodb.net/";
-const DB_HOST = process.env.DB_HOST || "mongodb://127.0.0.1/";
+const DB_HOST = process.env.DB_HOST || "mongodb+srv://charlesflorescalle:gkicdK2VPIDZ0Q0h@cluster0.saz6nxn.mongodb.net/";
+// const DB_HOST = process.env.DB_HOST || "mongodb://127.0.0.1/";
 const DB_DATABASE = process.env.DB_DATABASE || "pasanaku";
 const DB_USER = process.env.DB_USER || "charlesflorescalle";
 const DB_PASSWORD = process.env.DB_PASSWORD || "gkicdK2VPIDZ0Q0h";
