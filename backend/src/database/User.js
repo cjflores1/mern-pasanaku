c2ea10e4f8c9d716e7774001e543c5aac053079b
Charles Flores <charlesflorescalle@gmail.com>
2025-02-18 15:48:15 -0400

    Update users 2

--- backend/src/database/User.js
+++ backend/src/database/User.js
@@ -3,8 +3,12 @@ const User = require("./models/User");
 const user1 = new User({name: "Delia", position: 1, year: 2025, status: true});
 const user2 = new User({name: "Zulma", position: 2, year: 2025, status: true});
 const user3 = new User({name: "Christian", position: 3, year: 2025, status: true});
+const user4 = new User({name: "Mayra", position: 5, year: 2025, status: true});
+const user5 = new User({name: "Mayra", position: 9, year: 2025, status: true});
+const user6 = new User({name: "Jael Marin", position: 6, year: 2025, status: true});
+const user7 = new User({name: "Rocio Bellido", position: 7, year: 2025, status: true});
 
-let users = [user1, user2, user3];
+let users = [user1, user2, user3, user4, user5, user6, user7];
 
 const find = (userToFind) => {
   if(userToFind["position"]) {
